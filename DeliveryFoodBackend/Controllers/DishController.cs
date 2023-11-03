efbcbe9dcb4960d1036efa82ed8a3cdf1e9b7391
Yuriy Eliseev <yurij.eliseev.2004@mail.ru>
2023-11-03 21:16:55 +0700

    Реализовал получение списка блюд (Меню)
    Расписал логику получения меню в сервисе, сделал проверку на страницу, сортировку и категорию, что в них не вписаны значения вызывающие Exception, заполнил таблицу Dishes данными и расписал контроллер запроса

--- DeliveryFoodBackend/Controllers/DishController.cs
+++ DeliveryFoodBackend/Controllers/DishController.cs
@@ -1,8 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DeliveryFoodBackend.Data.Models.Enums;
+using DeliveryFoodBackend.DTO;
+using DeliveryFoodBackend.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DeliveryFoodBackend.Controllers
 {
+    [ApiController]
+    [Route("api/dish")]
     public class DishController : ControllerBase
     {
+        public readonly IDishService _dishService;
+
+        public DishController(IDishService dishService)
+        {
+            _dishService = dishService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetDishList(DishCategory? category, bool vegetarian, DishSorting? sorting, int page)
+        {
+            try
+            {
+                DishPagedListDto dishPagedList = await _dishService.GetListDishes(category, vegetarian, sorting, page);
+                return Ok(dishPagedList);
+            }
+            catch (BadHttpRequestException ex)
+            {
+                return BadRequest(new Response
+                {
+                    Status = "Error",
+                    Message = ex.Message
+                });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new Response
+                {
+                    Status = "Error",
+                    Message = ex.Message
+                });
+            }
+        }
     }
 }
--- DeliveryFoodBackend/Services/Interfaces/IDishService.cs
+++ DeliveryFoodBackend/Services/Interfaces/IDishService.cs
@@ -1,6 +1,10 @@
-﻿namespace DeliveryFoodBackend.Services.Interfaces
+﻿using DeliveryFoodBackend.Data.Models.Enums;
+using DeliveryFoodBackend.DTO;
+
+namespace DeliveryFoodBackend.Services.Interfaces
 {
     public interface IDishService
     {
+        Task<DishPagedListDto> GetListDishes(DishCategory? category, bool vegetarian, DishSorting? sorting, int page);
     }
 }
